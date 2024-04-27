306429f02326bd4aa123b8da9accf5cd272548fb
baocoder130703 <bphung08@gmail.com>
2024-04-27 14:30:46 +0700

    project

---
+++ src/main/java/banhang/quanlythucpham/qdl/QdlIndex.java
@@ -0,0 +1,35 @@
+package banhang.quanlythucpham.qdl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class QdlIndex {
+
+    @GetMapping({"/", "/trangchu"})
+    public String indexAction(Model model, HttpSession session, HttpServletRequest request) {
+        
+        model.addAttribute("content", "KhachHang/index.html");
+
+        return "KhachHang/layout.html"; 
+    }
+
+    @GetMapping({"/admin"})
+    public String adminAction(Model model, HttpSession session, HttpServletRequest request) {
+
+        if(session.getAttribute("USER_LOGGED")==null)
+        {
+            request.getSession().setAttribute("LOCATION","/admin");
+            return "redirect:/admin/dangnhap";
+        }
+
+        model.addAttribute("content", "QuanTri/index.html");
+        return "QuanTri/layout.html"; 
+
+    }
+
+}
